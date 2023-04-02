312cf5043d0ea4fd16d482c283cdd74ccead08de
Kpokharel3 <krishna.pokharel@elscript.com>
2023-04-03 01:36:11 +0545

    global variable created

---
+++ global/Button.tsx
@@ -0,0 +1,11 @@
+// import Link from "next/link"
+
+// const Button = ({type="submit",onEvent,disable,title,name,path}:any) => {
+//   return (
+//     <Link href={path} className="p-2 bg-primary " disabled={disable} title="title" onClick={onEvent}>
+//       {name}
+//     </Link>
+//   )
+// }
+
+// export default Button
