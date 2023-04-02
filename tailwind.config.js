965e1bf231cf5d7e853a65d68a4cc5642b6308f5
Kpokharel3 <krishna.pokharel@elscript.com>
2023-04-03 01:35:58 +0545

    primary color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,16 @@ module.exports = {
     
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
 
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:"#299e8c",
+        // primarysec:"#e8b715"
+      }
+    },
   },
   plugins: [],
 }
