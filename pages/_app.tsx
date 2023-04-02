f6e8cab11efa70d3c4f27d89a6f543e281442271
Kpokharel3 <krishna.pokharel@elscript.com>
2023-04-03 01:36:31 +0545

    imported google icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
