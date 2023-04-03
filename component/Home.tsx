4501016aa06745dece185ac7b36e3e43beb98933
Kpokharel3 <krishna.pokharel@elscript.com>
2023-04-04 01:45:31 +0545

    home component created

---
+++ component/Home.tsx
@@ -0,0 +1,14 @@
+
+import Hero from "./home/hero/Hero"
+import Navbar from "./home/navbar/Navbar"
+const Home = () => {
+  return (
+    <div>
+
+        <Navbar/>
+        <Hero/>
+    </div>
+  )
+}
+
+export default Home
