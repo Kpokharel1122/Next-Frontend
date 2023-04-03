48ea3b9dfc5a83789b2a5ef714643669988a9c3b
Kpokharel3 <krishna.pokharel@elscript.com>
2023-04-04 01:46:11 +0545

    uncomented code

--- global/Button.tsx
+++ global/Button.tsx
@@ -1,11 +1,11 @@
-// import Link from "next/link"
+import Link from "next/link"
 
-// const Button = ({type="submit",onEvent,disable,title,name,path}:any) => {
-//   return (
-//     <Link href={path} className="p-2 bg-primary " disabled={disable} title="title" onClick={onEvent}>
-//       {name}
-//     </Link>
-//   )
-// }
+const Button = ({type="submit",onEvent,disable,title,name,path}:any) => {
+  return (
+    <Link href={path} className="p-2 bg-primary " disabled={disable} title="title" onClick={onEvent}>
+      {name}
+    </Link>
+  )
+}
 
-// export default Button
+export default Button
